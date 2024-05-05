4d2ed44d2bd0157f8d96d0e8efe59e531a1a1980
argyris3 <78222306+argyris3@users.noreply.github.com>
2024-05-05 12:17:39 +0300

    fourthcommit

--- backend/server.js
+++ backend/server.js
@@ -39,12 +39,12 @@ app.use(express.static(path.join(__dirname, './uploads')));
 app.use(express.static(path.join(__dirname, './frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend', './dist', './index.html'));
+  res.sendFile(path.join(__dirname, '/frontend', '/dist', '/index.html'));
 });
 
-app.use('./uploads', express.static(path.join(__dirname + './uploads')));
+app.use('./uploads', express.static(path.join(__dirname + '/uploads')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, () => {
