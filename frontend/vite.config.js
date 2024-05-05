e535c3784a300ed76e5fb984250eb537555abaf9
argyris3 <bigol3@hotmail.com>
2024-05-05 17:04:40 +0300

    sixth commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/backend/": "http://localhost:4001",
+      "/uploads/": "http://localhost:4001"
+    }
+  }
 })
