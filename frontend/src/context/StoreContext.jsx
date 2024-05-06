ce409dd9fcecbe9a5c45a5bf7e9ea47e26c5ab74
argyris3 <78222306+argyris3@users.noreply.github.com>
2024-05-06 15:46:44 +0300

    nine commit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = 'http://localhost:4001';
+  const url = '';
   const [token, setToken] = useState('');
   const [cocktail_list, setCocktailList] = useState([]);
 
